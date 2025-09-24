02be902f4fb53543b9b1e09a5c0ce7c0bcc23a18
antonydp <38143733+antonydp@users.noreply.github.com>
2025-09-24 11:45:57 +0200

    refactor: Update layout and styling in AttributeBlock for improved readability and consistency

--- components/shared/AttributeBlock.tsx
+++ components/shared/AttributeBlock.tsx
@@ -16,23 +16,25 @@ interface AttributeBlockProps<T extends Record<string, any>> {
 export const AttributeBlock = <T extends Record<string, any>>({ name, value, onChange, isPoolExhausted, max, minValue = 0, className, baseValue }: AttributeBlockProps<T>) => {
 
     return (
-        <div className={`flex flex-col items-center py-5 px-2 rounded-lg ${className}`} style={{ backgroundColor: '#00a6ff99' }}>
-            <span className="font-bold text-white uppercase mb-1">{name}</span>
-            <div className="flex items-center justify-center w-full">
-                <CircleRating
-                    value={value}
-                    max={max}
-                    onChange={(v) => {
-                        if (v >= minValue && v <= max) {
-                            onChange(v);
-                        }
-                    }}
-                    isPoolExhausted={isPoolExhausted}
-                    circleClassName="w-4 h-4"
-                    className="gap-1.5"
-                    baseValue={baseValue}
-                />
-            </div>
-        </div>
-    );
+    <div 
+        className={`flex flex-col items-center p-6 gap-2 rounded-lg ${className}`} 
+        style={{ backgroundColor: '#00a6ff99' }}
+    >
+        <span className="font-bold text-white uppercase text-center">{name}</span>
+
+        <CircleRating
+            value={value}
+            max={max}
+            onChange={(v) => {
+                if (v >= minValue && v <= max) {
+                    onChange(v);
+                }
+            }}
+            isPoolExhausted={isPoolExhausted}
+            circleClassName="w-4 h-4"
+            className="gap-1.5"
+            baseValue={baseValue}
+        />
+    </div>
+);
 };
